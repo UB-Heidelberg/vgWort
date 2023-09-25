97c35c7bbfa0ad99266ca2f43175ab6d6979be6d
sf409 <otte@129.206.4.71>
2023-09-25 13:18:10 +0200

    Regular expression updated

--- VgwortPlugin.php
+++ VgwortPlugin.php
@@ -764,9 +764,9 @@ class VgwortPlugin extends GenericPlugin {
                                 $bookManuscriptFile->getId()
                             ]
                         );
-                        $search = '#<a (.+)href="' . $publicationFormatUrl . '"(.+)>#';
+                        $search = '#<a (.*)href="' . $publicationFormatUrl . '"(.*)>#';
                         // insert pixel tag for galleys download links using JS
-                        $replace = '<div style="font-size:0;line-height:0;width:0;" id="div_vgwpixel_' . $publicationFormat->getId() . '"></div><a class="$1" href="' . $publicationFormatUrl . '" onclick="vgwPixelCall(' . $publicationFormat->getId() . ');">';
+                        $replace = '<div style="font-size:0;line-height:0;width:0;" id="div_vgwpixel_' . $publicationFormat->getId() . '"></div><a $1 $2 href="' . $publicationFormatUrl . '" onclick="vgwPixelCall(' . $publicationFormat->getId() . ');">';
                         // insert pixel tag for galleys download links using VG Wort redirect
                         $output = preg_replace($search, $replace, $output);
                     }
